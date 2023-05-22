abed24c94af91b28e8f42ce4c444a3c8c6ba34db
Tom Mason <wheybags@wheybags.com>
2023-05-22 23:59:38 +0200

    better background window ensure sequence

--- EmperorHooks/dllmain.cpp
+++ EmperorHooks/dllmain.cpp
@@ -14,6 +14,9 @@
 #include "PatchDebugLog.hpp"
 #include "PatchSettings.hpp"
 #include "PatchLan.hpp"
+#include <string_view>
+#include <winternl.h>
+#include <Ntstatus.h>
 
 
 int (WINAPI* TrueShowCursor)(BOOL bShow) = ShowCursor;
@@ -55,7 +58,6 @@ void setupConsole()
   }
 }
 
-bool isBorderlessWindowed = true;
 
 volatile HWND backgroundWindowHandle = nullptr;
 volatile DWORD backgroundWindowThreadId = 0;
@@ -72,112 +74,26 @@ typedef BOOL(__cdecl* SetWindowStyleAndDrainMessagesFuncType)(HWND hWnd, int wid
 SetWindowStyleAndDrainMessagesFuncType setWindowStyleAndDrainMessagesOriginal = (SetWindowStyleAndDrainMessagesFuncType)0x004A7260;
 BOOL __cdecl setWindowStyleAndDrainMessages(HWND hWnd, int width, int height, char windowedMode)
 {
-  DWORD style; // ebp
-  int useWidth; // esi
-  int useHeight; // ebx
-  BOOL hasWindow; // esi
-  DWORD extendedStyle; // eax
-  BOOL result; // eax
-  struct tagRECT rc; // [esp+10h] [ebp-2Ch] BYREF
-  struct tagMSG Msg; // [esp+20h] [ebp-1Ch] BYREF
+  SetWindowLongA(hWnd, GWL_STYLE, WS_POPUP);
+  int left = GetSystemMetrics(SM_CXSCREEN) / 2 - width / 2;
+  SetWindowPos(hWnd, HWND_TOPMOST, left, 0, width, height, SWP_SHOWWINDOW);
 
-  style = WS_POPUP;
-  useWidth = GetSystemMetrics(SM_CXSCREEN);
-  useHeight = GetSystemMetrics(SM_CYSCREEN);
-  if (windowedMode)
-  {
-    useWidth = width;
-    useHeight = height;
-    if (!isBorderlessWindowed)
-      style = WS_POPUPWINDOW | WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_DLGFRAME;
-  }
-  SetWindowLongA(hWnd, GWL_STYLE, style);
-  if (isBorderlessWindowed)
-  {
-    int left = GetSystemMetrics(SM_CXSCREEN) / 2 - useWidth / 2;
-    SetWindowPos(hWnd, HWND_TOPMOST, left, 0, useWidth, useHeight, SWP_SHOWWINDOW);
-
-    RECT rc = {};
-    GetWindowRect(*mainWindowHandleP, &rc);
-    myClipCursor(&rc);
-  }
-  else
-  {
-    SetRect(&rc, 0, 0, useWidth, useHeight);
-    hasWindow = GetMenu(hWnd) != 0;
-    extendedStyle = GetWindowLongA(hWnd, GWL_EXSTYLE);
-    AdjustWindowRectEx(&rc, style, hasWindow, extendedStyle);
-    MoveWindow(hWnd, 0, 0, rc.right - rc.left, rc.bottom - rc.top, 0);
-    ShowWindow(hWnd, SW_SHOW);
-    InvalidateRect(hWnd, 0, 0);
-  }
+  RECT rc = {};
+  GetWindowRect(*mainWindowHandleP, &rc);
+  myClipCursor(&rc);
   
-  for (result = PeekMessageA(&Msg, 0, 0, 0, 0); result; result = PeekMessageA(&Msg, 0, 0, 0, 0))
+  BOOL result;
+  MSG msg;
+  for (result = PeekMessageA(&msg, 0, 0, 0, 0); result; result = PeekMessageA(&msg, 0, 0, 0, 0))
   {
-    GetMessageA(&Msg, 0, 0, 0);
-    if (Msg.message == WM_QUIT && (Msg.hwnd == *mainWindowHandleP || (HWND)Msg.wParam == *mainWindowHandleP))
+    GetMessageA(&msg, 0, 0, 0);
+    if (msg.message == WM_QUIT && (msg.hwnd == *mainWindowHandleP || (HWND)msg.wParam == *mainWindowHandleP))
       *gDoQuitP = 1;
-    TranslateMessage(&Msg);
-    DispatchMessageA(&Msg);
+    TranslateMessage(&msg);
+    DispatchMessageA(&msg);
   }
   return result;
 }
-
-typedef int(__stdcall* WndProcDuneIIIType)(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
-WndProcDuneIIIType wndProcDuneIIIOriginal = (WndProcDuneIIIType)0x004A6560;
-
-
-int __stdcall wndProcDuneIII(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
-{
-  int ret = wndProcDuneIIIOriginal(hWnd, Msg, wParam, lParam);
-
-
-  if (hWnd == *mainWindowHandleP && Msg == WM_ACTIVATEAPP)
-  {
-    if (wParam || (!wParam && lParam == backgroundWindowThreadId))
-    {
-      printf("GAINED FOCUS\n");
-      focus = true;
-    }
-    else
-    {
-      printf("LOST FOCUS\n");
-      focus = false;
-      SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
-      ShowWindow(hWnd, SW_MINIMIZE);
-
-      SetWindowPos(backgroundWindowHandle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
-      ShowWindow(backgroundWindowHandle, SW_MINIMIZE);
-    }
-  }
-
-  static bool hadFocus = true;
-
-  if (focus && !hadFocus)
-  {
-    RECT rc = {};
-    GetWindowRect(*mainWindowHandleP, &rc);
-    myClipCursor(&rc);
-
-    std::thread([]()
-    {
-      printf("%p %p\n", *mainWindowHandleP, backgroundWindowHandle);
-      Sleep(1000 * 2);
-      ShowWindow(backgroundWindowHandle, SW_SHOWNOACTIVATE);
-      Sleep(1000 * 2);
-      SetWindowPos(backgroundWindowHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
-      Sleep(1000 * 2);
-      SetWindowPos(*mainWindowHandleP, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW);
-      ShowWindow(*mainWindowHandleP, SW_SHOW);
-    }).detach();
-  }
-
-  hadFocus = focus;
-
-  return ret;
-}
-
-
 
 void backgroundWindow()
 {
@@ -232,6 +148,163 @@ void backgroundWindow()
   }
 }
 
+struct SYSTEM_PROCESS_ID_INFORMATION
+{
+  HANDLE ProcessId;
+  UNICODE_STRING ImageName;
+};
+
+constexpr SYSTEM_INFORMATION_CLASS SystemProcessIdInformation = SYSTEM_INFORMATION_CLASS(0x58);
+
+NTSTATUS GetProcessPathById(DWORD ProcessId, wchar_t** output)
+{
+  NTSTATUS status = S_OK;
+
+  HMODULE ntdll = nullptr;
+  NTSTATUS(*NtQuerySystemInformation)(SYSTEM_INFORMATION_CLASS SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength, PULONG ReturnLength) = nullptr;
+
+  SYSTEM_PROCESS_ID_INFORMATION spii = { (HANDLE)ProcessId, { .Length = 0, .MaximumLength = 4096, .Buffer = nullptr } };
+
+
+  ntdll = LoadLibraryA("ntdll.dll");
+  if (!ntdll)
+  {
+    status = GetLastError();
+    goto error;
+  }
+
+  NtQuerySystemInformation = (NTSTATUS (*)(SYSTEM_INFORMATION_CLASS SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength, PULONG ReturnLength))GetProcAddress(ntdll, "NtQuerySystemInformation");
+  if (!NtQuerySystemInformation)
+  {
+    status = GetLastError();
+    goto error;
+  }
+
+  spii.ImageName.Buffer = (PWSTR)LocalAlloc(LMEM_FIXED, spii.ImageName.MaximumLength);
+  if (!spii.ImageName.Buffer)
+  {
+    status = GetLastError();
+    goto error;
+  }
+
+  status = NtQuerySystemInformation(SystemProcessIdInformation, &spii, sizeof(spii), 0);
+  if (status == STATUS_INFO_LENGTH_MISMATCH)
+  {
+    LocalFree(spii.ImageName.Buffer);
+    spii.ImageName.Buffer = (PWSTR)LocalAlloc(LMEM_FIXED, spii.ImageName.MaximumLength);
+    if (!spii.ImageName.Buffer)
+    {
+      status = GetLastError();
+      goto error;
+    }
+  }
+
+  if (status != S_OK)
+    goto error;
+
+  *output = spii.ImageName.Buffer;
+  goto end;
+
+error:
+  if (spii.ImageName.Buffer)
+    LocalFree(spii.ImageName.Buffer);
+
+end:
+  if (ntdll)
+    FreeLibrary(ntdll);
+
+  return status;
+}
+
+void windowSwitchHandlerLoop()
+{
+  while (!*mainWindowHandleP || !backgroundWindow)
+    Sleep(10);
+
+  HWND lastForeground = nullptr;
+  bool force = true;
+
+
+  while (true)
+  {
+    Sleep(1000);
+
+    HWND foreground = GetForegroundWindow();
+
+    DWORD dwProcId = 0;
+    if (!GetWindowThreadProcessId(foreground, &dwProcId))
+    {
+      printf("GetWindowThreadProcessId failed %d\n", GetLastError());
+    }
+    else
+    {
+      wchar_t* processPath = nullptr; 
+      NTSTATUS status = GetProcessPathById(dwProcId, &processPath);
+      if (status != S_OK)
+      {
+        printf("GetProcessPathById failed %d\n", status);
+      }
+      else
+      {
+        //printf("fg %d %d %S !!!\n", DWORD(foreground), dwProcId, processPath);
+
+        // sometimes during loading dwm.exe / WerFault.exe take over as the "Game.exe is not responding" window
+        bool isDwmExe = std::wstring_view(processPath).ends_with(L"\\dwm.exe");
+        bool isWerFaultExe = std::wstring_view(processPath).ends_with(L"\\WerFault.exe");
+
+        LocalFree(processPath);
+
+        if (isDwmExe || isWerFaultExe)
+          continue;
+      }
+    }
+
+    if (foreground == backgroundWindowHandle)
+    {
+      printf("bg was fg, fixing\n");
+      ShowWindow(*mainWindowHandleP, SW_SHOW);
+      SetForegroundWindow(*mainWindowHandleP);
+      lastForeground = *mainWindowHandleP;
+      Sleep(1000 * 3);
+      continue;
+    }
+    else if ((foreground == *mainWindowHandleP && lastForeground != *mainWindowHandleP) || force)
+    {
+      printf("SHOW START\n");
+
+      printf("    topmost bg\n");
+      SetWindowPos(backgroundWindowHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+      printf("    show bg\n");
+      ShowWindow(backgroundWindowHandle, SW_SHOWMAXIMIZED);
+      SetForegroundWindow(backgroundWindowHandle);
+
+      //Sleep(1000 * 3);
+
+      printf("    topmost main\n");
+      SetWindowPos(*mainWindowHandleP, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_SHOWWINDOW);
+      printf("    show main\n");
+      ShowWindow(*mainWindowHandleP, SW_SHOW);
+      SetForegroundWindow(*mainWindowHandleP);
+
+      //Sleep(1000 * 3);
+
+      printf("SHOW DONE\n");
+      force = false;
+    }
+    else if (foreground != *mainWindowHandleP && lastForeground == *mainWindowHandleP)
+    {
+      printf("HIDE\n");
+      SetWindowPos(*mainWindowHandleP, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+      ShowWindow(*mainWindowHandleP, SW_MINIMIZE);
+
+      SetWindowPos(backgroundWindowHandle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+      ShowWindow(backgroundWindowHandle, SW_MINIMIZE);
+    }
+
+    lastForeground = foreground;
+  }
+}
+
 __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 {
   if (DetourIsHelperProcess()) {
@@ -240,9 +313,6 @@ __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOI
 
   if (dwReason == DLL_PROCESS_ATTACH) 
   {
-    MessageBoxA(nullptr, "AAA", "AAA", 0);
-
-
     DWORD doFullscreen = 1;
     DWORD size = sizeof(DWORD);
     HRESULT result = RegGetValueA(HKEY_CURRENT_USER, "Software\\WestwoodRedirect\\Emperor\\LauncherCustomSettings", "DoFullscreen", RRF_RT_REG_DWORD, nullptr, &doFullscreen, &size);
@@ -259,10 +329,8 @@ __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOI
     DetourAttach(&(PVOID&)OutputDebugStringWReal, OutputDebugStringWWrap);
     //DetourAttach(&(PVOID&)TrueShowCursor, FakeShowCursor);
     if (doFullscreen)
-    {
       DetourAttach(&(PVOID&)setWindowStyleAndDrainMessagesOriginal, setWindowStyleAndDrainMessages);
-      DetourAttach(&(PVOID&)wndProcDuneIIIOriginal, wndProcDuneIII);
-    }
+
     DetourAttach(&(PVOID&)doCdCheckOrig, doCdCheckPatched);
     DetourAttach(&(PVOID&)regSettingsOpenHkeyOrig, regSettingsOpenHkeyPatched);
     HookD3D7();
@@ -275,6 +343,7 @@ __declspec(dllexport) BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOI
     if (doFullscreen)
     {
       std::thread([]() { backgroundWindow(); }).detach();
+      std::thread([]() { windowSwitchHandlerLoop(); }).detach();
       std::thread([]()
       {
         while (true)
