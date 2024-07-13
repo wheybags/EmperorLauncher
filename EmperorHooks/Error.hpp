003d26a2689eaabcd5886ac414fe5f649b34ce1e
Tom Mason <wheybags@wheybags.com>
2024-07-13 20:44:45 +0200

    show assert info

--- EmperorHooks/Error.hpp
+++ EmperorHooks/Error.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
-#define release_assert(X) if (!(X)) do { MessageBoxA(nullptr, "assert failed", "assert failed", 0); abort(); } while (0)
+#define RASSERT_STRINGIFY(X) #X
+#define RASSERT_STRINGIFY_2(X) RASSERT_STRINGIFY(X)
+
+#define release_assert(X) if (!(X)) do { MessageBoxA(nullptr, "assert failed: " RASSERT_STRINGIFY(X) " at " __FILE__ ":" RASSERT_STRINGIFY_2(__LINE__), "assert failed", 0); abort(); } while (0)
